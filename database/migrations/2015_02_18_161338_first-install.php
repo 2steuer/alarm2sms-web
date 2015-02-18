fef002d5f94c208bb79a6414bc8cdb49eb52cce2
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2015-02-19 00:20:31 +0100

    Adding install migration

---
+++ database/migrations/2015_02_18_161338_first-install.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FirstInstall extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('persons', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->string('number');
+                $table->boolean('flash');
+            });
+
+        Schema::create('groups', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->text('description');
+            });
+
+        Schema::create('group_person', function(Blueprint $table) {
+                $table->integer('group_id')->unsigned();
+                $table->integer('person_id')->unsigned();
+                $table->integer('order');
+                $table->foreign('group_id')->references('id')->on('groups');
+                $table->foreign('person_id')->references('id')->on('persons');
+            });
+
+        Schema::create('triggers', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->text('description');
+            });
+
+        Schema::create('triggerslots', function(Blueprint $table) {
+                $table->increments('id');
+                $table->text('text');
+                $table->integer('weekday');
+                $table->time('start');
+                $table->time('end');
+                $table->integer('trigger_id')->unsigned();
+                $table->foreign('trigger_id')->references('id')->on('triggers');
+            });
+
+        Schema::create('group_triggerslot', function(Blueprint $table) {
+                $table->integer('group_id')->unsigned();
+                $table->integer('triggerslor_id')->unsigned();
+                $table->integer('order');
+                $table->foreign('group_id')->references('id')->on('groups');
+            });
+
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	    Schema::drop('group_person');
+        Schema::drop('group_triggerslot');
+        Schema::drop('triggerslots');
+        Schema::drop('trigger');
+        Schema::drop('groups');
+        Schema::drop('persons');
+	}
+
+}
