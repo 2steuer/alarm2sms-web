6c74213aedbc0de46aba8abd20f6971395a9964d
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2015-02-19 00:53:41 +0100

    Beginning layouting

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <base href="{{ env('BASE_PATH) }}" />
+
+        <title>{{ env('SITE_TITLE') }} &raquo; {{ $title }}</title>
+
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    </head>
+<body>
+
+
+
+<script src="js/jquery-2.1.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
