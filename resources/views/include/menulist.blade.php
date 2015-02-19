20f8b5181b153af208b44c4051357b032f48ee69
Merlin Steuer <2steuer@informatik.uni-hamburg.de>
2015-02-19 01:12:49 +0100

    adding menu list include

---
+++ resources/views/include/menulist.blade.php
@@ -0,0 +1,6 @@
+<ul class="{{ $nav_classes }}">
+    <li><a href="/home">Startseite</a></li>
+    <li><a href="/persons">Personen verwalten</a></li>
+    <li><a href="/groups">Gruppen verwalten</a></li>
+    <li><a href="/triggers">Auslöser verwalten</a></li>
+</ul>
